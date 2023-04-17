fb4f1aa510991e4b1e5cc56b7e335814ed79fb75
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-04-17 15:41:59 +0200

    rebase always for pull

--- lua/remaps/fugitive.lua
+++ lua/remaps/fugitive.lua
@@ -3,11 +3,11 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
 -- push remap
 local bufnr = vim.api.nvim_get_current_buf()
 local opts = {buffer = bufnr, remap = false}
-vim.keymap.set("n", "<leader>p", function()
+vim.keymap.set("n", "<leader>gp", function()
     vim.cmd.Git('push')
 end, opts)
 
--- merge always
+-- rebase always for pull
 vim.keymap.set("n", "<leader>P", function()
-    vim.cmd.Git({'pull', '--merge'})
+    vim.cmd.Git({'pull', '--rebase'})
 end, opts)
