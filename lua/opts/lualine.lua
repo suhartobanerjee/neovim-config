6437640fdf33735eb77007d64aded71949772483
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-04-18 22:44:14 +0200

    modified lualine
    added buffers and buf# as well as total number of lines instead of
    progress.

--- lua/opts/lualine.lua
+++ lua/opts/lualine.lua
@@ -19,17 +19,19 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_b = {'branch'},
+    lualine_c = {
+        {'buffers', mode = 4,},
+    },
+    lualine_x = {'filetype'},
+    lualine_y = {"vim.api.nvim_buf_line_count(0)"},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
