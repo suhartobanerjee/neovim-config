501d1f783fe8d5724cd8a0f26a82617fd25b9138
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-04-28 14:16:10 +0200

    added remaps for block commenting

--- lua/remaps/nvim-R.lua
+++ lua/remaps/nvim-R.lua
@@ -5,19 +5,25 @@ function customNvimRMappings()
 
    vim.keymap.set({'n', 'v', 'i'}, '<localleader>m',  ' %>% ', {buffer = 0, remap = true}) 
 
-   vim.keymap.set({'n', 'v', 'i'}, '<localleader>c', '<Esc><Plug>RToggleCommenti<C-o>$', {buffer = 0, remap = true})
+   vim.keymap.set({'n', 'v', 'i'}, '<localleader>cl', '<Esc><Plug>RToggleCommenti<C-o>$', {buffer = 0, remap = true})
 
    vim.keymap.set({'n', 'v', 'i'}, '<localleader>ro', '<Esc><Plug>RUpdateObjBrowseri', {buffer = 0, remap = true})
 
    vim.keymap.set({'n', 'v', 'i'}, '<localleader>l', '<Esc><Plug>RClearConsolei', {buffer = 0, remap = true})
 
    -- Keymap to save the current plot to the tmp_plot loc
-   vim.keymap.set('i', '<localleader>ss', '<CR>ggsave(tmp_plot)')
+   vim.keymap.set({'i', 'n'}, '<localleader>ss', 'ggsave(tmp_plot)')
 
    vim.keymap.set('n', '<leader>rz', ':qa!<CR>')
 
    -- keymap to write traceback(), run it and then delete the line
    vim.keymap.set('i', '<localleader>tb', 'traceback()<Esc><Plug>RSendLineS')
+
+   -- keymap to comment multiple lines
+   vim.keymap.set('v', '<localleader>cb', ':s/^/# /<CR>:noh<CR>', {silent = true})
+
+   -- keymap to uncomment multiple lines
+   vim.keymap.set('v', '<localleader>ub', ':s/^# //<CR>:noh<CR>', {silent = true})
 
 end
 
