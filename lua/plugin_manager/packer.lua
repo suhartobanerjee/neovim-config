b88ab7e37fc45d94d7888f356cba2151f154780b
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-12-11 00:02:51 +0100

    Fixed telescope issue
    Just bumped the version, and it was resolved!

--- lua/plugin_manager/packer.lua
+++ lua/plugin_manager/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
 
     -- 1. telescope (fuzzy finder)
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
