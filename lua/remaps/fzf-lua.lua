1a88add68571f2907114052acf2336b0058aeab8
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2024-08-22 09:19:55 +0200

    fzf-lua requires ripgrep and bat to be installed. cool, done.

--- lua/remaps/fzf-lua.lua
+++ lua/remaps/fzf-lua.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>ff", "<Esc>:call fzf#run(fzf#wrap({'options': ['--preview', 'bat {}']}))<CR>")
+-- vim.keymap.set("n", "<leader>fc", "<Esc>:call fzf#run(fzf#wrap({'options': ['--preview', 'bat {}']}))<CR>")
+vim.keymap.set("n", "<leader>ff", ":lua require('fzf-lua').files()<CR>")
